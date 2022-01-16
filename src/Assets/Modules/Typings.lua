19563f099b610d312009f3369d76290bf8977f4e
7kayoh <nakogls@outlook.com>
2022-01-16 18:24:17 +0900

    Added new Typings and changed name of RemoteBodyEntry

--- src/Assets/Modules/Typings.lua
+++ src/Assets/Modules/Typings.lua
@@ -1,16 +1,16 @@
 export type MainConfig = {
     Administration: {
         Users: {
-            {Type: string,
-            Value: string,
-            Group: string}
+            {Type: string, -- Type of UserEntry
+            Value: string, -- Value that accepts
+            Group: string} -- Which group it belongs to
         },
         Groups: {
-            {Id: string,
-            Name: string,
-            Commands: string|{string}}
+            {Id: string, -- Special identifier for the group, must not have space or any symbols
+            Name: string, -- Name for the group
+            Commands: string|{string}} -- Commands allowed to use, "*" for all commands
         },
-        CommandFeatures: {any}, -- Honestly, we do not know what will be in there
+        CommandFeatures: {any}, -- Additional configuration for a specific command
     },
 
     Interface: {
@@ -33,6 +33,20 @@ export type MainConfig = {
             Key: string
         }
     }
+}
+
+export type RemoteBody = {
+    Method: string, -- The method (what are you calling)
+    Attachment: {any}? -- Attachment for that menthod
+}
+
+export type BasePackage = {
+    Name: string, -- Name of package
+    Id: string, -- Unique identifier of package, must not include space or symbols
+    Description: string?, -- Description of package, optional
+    Category: string, -- Category of the package, usable for commands
+    Type: string, -- Type of the package: Commands/Extensions/Stylesheets
+    Container: {any} -- Container for the package, can store functions or data for that package
 }
 
 local module = {}
