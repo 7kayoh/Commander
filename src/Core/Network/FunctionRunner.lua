7322197376cdfb745484c9fe2c3fd0713ddc16b2
7kayoh <nakogls@outlook.com>
2022-01-24 17:39:57 +0900

    Pass false and reason instead of pcall(error)

--- src/Core/Network/FunctionRunner.lua
+++ src/Core/Network/FunctionRunner.lua
@@ -15,7 +15,7 @@ Remotes.RemoteFunction.OnServerInvoke = function(player: Player, body: Typings.R
     if method then
         return pcall(method(player, body.data))
     else
-        return pcall(error(body.Method .. " is not a valid member of " .. Methods.Functions:GetFullName(), 2))
+        return false, body.Method .. " is not a valid member of " .. Methods.Functions:GetFullName()
     end
 end
 
