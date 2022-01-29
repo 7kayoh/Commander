5bd607ce5f0eda568e93ba7e1d037acc250a500d
7kayoh <nakogls@outlook.com>
2022-01-29 14:31:24 +0900

    Moved EventRunner and FunctionRunner into API

--- src/Core/Network/init.lua
+++ src/Core/Network/init.lua
@@ -2,12 +2,33 @@
 -- 7kayoh
 -- Jan 16, 2022
 
+local MainAPI = require(script.Parent.Main)
+local Methods = script.RemoteMethods
+local Assets = MainAPI.Assets
+
+local Remotes = MainAPI.Remotes
+local Typings = require(Assets.Modules.Typings)
+
 local Network = {}
 
 function Network._onInit()
     Network._OnInit = nil
-    require(script.EventRunner)
-    require(script.FunctionRunner)
+    Remotes.RemoteFunction.OnServerInvoke = function(player: Player, body: Typings.RemoteBody): any?
+        local method = Methods.Functions:FindFirstChild(body.Method)
+        if method then
+            return pcall(method(player, body.data))
+        else
+            error(body.Method .. " is not a valid member of " .. Methods.Functions:GetFullName())
+        end
+    end
+    Remotes.RemoteEvent.OnServerEvent:Connect(function(player: Player, body: Typings.RemoteBody)
+        local method = Methods.Events:FindFirstChild(body.Method)
+        if method then
+            method(player, body.data)
+        else
+            error(3, body.Method .. " is not a valid member of " .. Methods.Events:GetFullName())
+        end
+    end)
 end
 
 return Network
