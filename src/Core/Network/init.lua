238cdff522a2915810de2769ccb5151094535bc4
7kayoh <nakogls@outlook.com>
2022-02-06 20:48:41 +0900

    Add .addMethod and fix :29

--- src/Core/Network/init.lua
+++ src/Core/Network/init.lua
@@ -9,7 +9,15 @@ local Assets = MainAPI.Assets
 local Remotes = MainAPI.Remotes
 local Typings = require(Assets.Modules.Typings)
 
-local Network = {}
+local Network = {
+    MethodTypes = { "Event", "Function" }
+}
+
+function Network.addMethod(remoteMethod: ModuleScript, methodType: string)
+    assert(table.find(Network.MethodTypes, methodType:lower()), "Invalid method type")
+    -- lmao im so lazy
+    remoteMethod:Clone().Parent = Methods[Network.MethodTypes[table.find(Network.MethodTypes, methodType:lower())]]
+end
 
 function Network._onInit()
     Network._OnInit = nil
@@ -21,12 +29,13 @@ function Network._onInit()
             error(body.Method .. " is not a valid member of " .. Methods.Functions:GetFullName())
         end
     end
+    
     Remotes.RemoteEvent.OnServerEvent:Connect(function(player: Player, body: Typings.RemoteBody)
         local method = Methods.Events:FindFirstChild(body.Method)
         if method then
             method(player, body.data)
         else
-            error(3, body.Method .. " is not a valid member of " .. Methods.Events:GetFullName())
+            error(body.Method .. " is not a valid member of " .. Methods.Events:GetFullName())
         end
     end)
 end
